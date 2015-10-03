517da45b822df6472d2118598447479c63e76c73
sabor413 <phiota413@gmail.com>
2015-10-03 18:46:15 -0400

    Continuing work with MvcMusicStore

---
+++ MvcMusicStore/DBContext/MusicStoreDB.cs
@@ -0,0 +1,18 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.DBContext
+{
+    public class MusicStoreDB : DbContext
+    {
+        public DbSet<AlbumInfo> AlbumInfos { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
